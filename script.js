f4b3e9dfd0ece393e4293ec2dccdcb82bdbe621d
Adrian <adriantutuianu01@gmail.com>
2021-05-23 00:04:11 +0100

    Added id's

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // select decimal, clear, backspace and total(result) buttons and save them in variables
-var decimalBtn = document.getElementsByClassName("dot");
-var clearBtn = document.getElementsByClassName("ac");
-var backspaceBtn = document.getElementsByClassName("backspace");
-var displayedResult = document.getElementsByClassName("result");
+var decimalBtn = document.getElementById("decimal");
+var clearBtn = document.getElementById("clear-btn");
+var backspaceBtn = document.getElementById("backspace");
+var displayedResult = document.getElementById("displayed-result");
 
 // select number buttons and save them in variables 0-9
 var calcNumBtns = document.getElementsByClassName("calc-btn-num");
 
 // select operator buttons and save them in variables +,-,*,/
-var plusBtn = document.getElementsByClassName("plus");
-var minusBtn = document.getElementsByClassName("minus");
-var multiplyBtn = document.getElementsByClassName("multiply");
-var divideBtn = document.getElementsByClassName("divide");
+var plusBtn = document.getElementById("plus-btn");
+var minusBtn = document.getElementById("minus-btn");
+var multiplyBtn = document.getElementById("multiply-btn");
+var divideBtn = document.getElementById("divide-btn");
+var equalBtn = document.getElementById("equal-btn");
 
 // declare variable to display 0 by default - on load, on reset and on of any operators
 var displayVal = "0";
@@ -24,6 +25,11 @@ var operationsArray = [];
 
 // create functions for all operations: plus, minus, multiply, divide & equal
 // push to array the pending value, operation and second value
+function calcPlus() {
+  console.log("ok");
+}
+
+plusBtn.addEventListener("click", calcPlus);
 
 // the equal function will use .join(' ')); // ['2', '+', '3'] => '2 + 3'
 
